1e1b3e2ff765984cc3d7b56719beb2a28502957b
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-03 23:37:43 +0500

    Update layers.js

--- js/main/layers.js
+++ js/main/layers.js
@@ -55,8 +55,8 @@ function AbsLayerum(n) {
   return (n.gte(tmp.layerRequired.pow(1000000)) ? "" : formatNumber(n.div(tmp.layerRequired.pow(n.log(tmp.layerRequired).floor())))) + " " + Layer(n.log(tmp.layerRequired))
 }
 function update() {
-  tmp.inc = tmp.number.add(0.0001).pow(1.001);
-  tmp.number = tmp.number.mul(1.001).pow(1.01).pow(tmp.inc);
+  tmp.inc = tmp.number.add(0.0001).pow(1.0001);
+  tmp.number = tmp.number.mul(1.001).pow(tmp.inc);
   tmp.layer = AbsLayerum(tmp.number);
   document.getElementById("app").innerHTML = `${tmp.layer}`;
 }
