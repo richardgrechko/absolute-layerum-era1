e62b2dbf63d64c0d2fbc454cf2c960750c0dbf2f
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-03 17:42:34 +0500

    Update layers.js

--- js/main/layers.js
+++ js/main/layers.js
@@ -10,19 +10,19 @@ let layers = [
 function Layer(n) {
   n = n.floor();
   let k = "";
-  if (n.gte(1)) {
-    k = layers[0][n];
-  } else if (n.gte(10)) {
-    k = `${layers[1]}<sup>${Layer(n.add(-10))}</sup>`
-  } else if (n.gte(100)) {
-    k = `${layers[2]}<sub>${Layer(n.add(-100))}</sub>`
-  } else if (n.gte(1000)) {
-    k = `${Layer(n.div(1000))}<sub>(${Layer(n.mod(1000))})</sub>`
-  } else if (n.gte(E(10).pow(9))) {
-    k = `${Layer(n.div(E(10).pow(9)))}<sup>{${Layer(n.mod(E(10).pow(9)))}}</sup>`
-  } else if (n.gte(E(10).pow(36))) {
+  if (n.gte(E(10).pow(36))) {
     let logBillion = n.log(E(10).pow(9));
     k = `<sup>{${Layer(n.log(E(10).pow(9)))}}</sup>${Layer(n.div(E(10).pow(9).pow(logBillion.floor())))}`
+  } else if (n.gte(E(10).pow(9))) {
+    k = `${Layer(n.div(E(10).pow(9)))}<sup>{${Layer(n.mod(E(10).pow(9)))}}</sup>`
+  } else if (n.gte(1000)) {
+    k = `${Layer(n.div(1000))}<sub>(${Layer(n.mod(1000))})</sub>`
+  } else if (n.gte(100)) {
+    k = `${layers[2]}<sub>${Layer(n.sub(-100))}</sub>`
+  } else if (n.gte(10)) {
+    k = `${layers[1]}<sup>${Layer(n.sub(-10))}</sup>`
+  } else if (n.gte(1)) {
+    k = layers[0][n];
   } else {
     console.error("Can't set a layer less than 1")
   }
