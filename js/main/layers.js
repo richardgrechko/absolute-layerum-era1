3d14387261b46f02665de380ce99c768803dfe4f
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-25 07:12:12 +0500

    Update layers.js

--- js/main/layers.js
+++ js/main/layers.js
@@ -1,52 +1,21 @@
 let tmp = {};
 tmp.number = E(0.0001);
-tmp.layerRequired = E(1.7976931348623157e+308);
+tmp.layerRequired = E(5);
 tmp.layer = "";
 let layers = [
-  ["Nul", "Un", "Bi", "Tr", "Te", "Pe", "He", "Hp", "Oc", "En"],
-  "Đe",
-  ["", "ϗ", "ϱ", "ϙ", "ͳ"],
-  " abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ",
-  " ☿♀♁♂♃♄♅♆♇",
+  "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ",
 ]
-function Layer_50(n) {
-  n = n.floor();
-  let m = "";
-  if (n.gte(5)) {
-    m = "ϸ<sup>" + Layer_50(n.sub(5)) + "</sup>";
-  } else if (n.gte(1)) {
-    m = layers[2][n];
-  } else {
-    m = " ";
-  }
-  return m;
-}
 function Layer(n) {
   n = n.floor();
   let k = "";
-  if (n.gte(E(10).tetrate(5e307))) {
-    let slog = n.slog(10);
-    k = "<|Ʊ|>:◯"
-  } else if (n.gte(E(10).tetrate(5))) {
-    let slog = n.slog(10);
-    k = "<|" + Layer(slog) + "|>:" + Layer(E(10).pow(E(10).pow(slog.sub(slog.floor()))))
-  } else if (n.gte(E(10).pow(E(10).pow(10)))) {
-    k = "<sub>{" + Layer(n.log(10)) + "}</sub>◯"
-  } else if (n.gte(E(10).pow(18))) {
-    let logMillion = n.log(E(1000000));
-    k = "<sub>{" + Layer(logMillion) + "}</sub>" + Layer(n.div(E(1000000).pow(logMillion.sub(1).floor())))
-  } else if (n.gte(1000000)) {
-    k = Layer(n.div(1000000)) + "<sub>(" + Layer(n.mod(1000000)) + ")</sub>"
-  } else if (n.gte(100000)) {
-    k = layers[4][n.div(100000).floor()] + "(" + Layer(n.mod(100000)) + ")"
-  } else if (n.gte(51000)) {
-    k = "Ω<sup>" + Layer(n.sub(50000)) + "</sup>"
-  } else if (n.gte(1000)) {
-    k = layers[3][n.div(1000).floor()] + "(" + Layer(n.mod(1000)) + ")"
-  } else if (n.gte(50)) {
-    k = Layer_50(n.div(50)) + "(" + Layer(n.mod(50)) + ")"
-  } else if (n.gte(10)) {
-    k = layers[1] + "<sup>" + Layer(n.sub(10)) + "</sup>"
+  if (n.gte(E(52).pow(1e10))) {
+    k = "[" + n.log(26).add(1).floor() + " letters]"
+  } else if (n.gte(52**25)) {
+    k = "[Layer " + n + ", " + n.log(26).add(1).floor() + " letters]"
+  } else if (n.gte(52**2)) {
+    k = Layer(n.div(52).floor()) + Layer(n.mod(52))
+  } else if (n.gte(52)) {
+    k = layers[0][n.div(52).floor()] + Layer(n.mod(52))
   } else if (n.gte(0)) {
     k = layers[0][n];
   } else {
