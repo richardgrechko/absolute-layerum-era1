61926253ae25f6f6cf6e9d346b1f2b077fb5ea87
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-26 13:39:56 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,12 +24,11 @@ const funcs = {
         alert("Error: " + e);
         return;
       }
-      tmp.number = loadVal(E(obj.number), E(0));
-      tmp.multi = loadVal(E(obj.multi), E(0));
-      tmp.rank = loadVal(E(obj.rank), E(0));
+      tmp.number = loadVal(E(obj.number), E(1));
+      tmp.multi = loadVal(E(obj.multi), E(1));
+      tmp.rank = loadVal(E(obj.rank), E(1));
       tmp.prestige = loadVal(E(obj.prestige), E(0));
       tmp.transcension = loadVal(E(obj.transcension), E(0));
-      loaded = true;
     }
   }
 }
