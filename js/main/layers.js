8fef929a61da211e8aeb997d4428a574206dcfe0
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-26 08:05:31 +0500

    Update layers.js

--- js/main/layers.js
+++ js/main/layers.js
@@ -39,8 +39,12 @@ function AbsLayerum(n) {
   return "<small style=\"color: #f77;\">Epilepsy warning when you get high stats!</small> "
   + "<small style=\"color: #7f7;\">This is based of Samir's AFK Incremental in Roblox!</small>"
   + "<p><small>You have </small>"
-  + AbsLayerumNotationHTML(n)
-  + " "
+  + (n.gte(tmp.layerRequired.pow(52*(53**9))) ? "1" : formatNumber(n.div(tmp.layerRequired.pow(n.log(tmp.layerRequired).floor()))))
+  + "<small style=\"color: "
+  + rainbowTransition(n.add(tmp.layerRequired).log(tmp.layerRequired).mul(5).floor().root(1.5))
+  + ";\">"
+  + Layer(n.log(tmp.layerRequired))
+  + ".</small> "
   + (tmp.number.gte(5) ? ("<tiny>This is also " + formatNumber(tmp.number) + " a.</tiny>") : "")
   + " <small>(+" 
   + formatNumber(tmp.statsPerSecond)
@@ -49,14 +53,6 @@ function AbsLayerum(n) {
 function AbsLayerumNotation(n) {
   return (n.gte(tmp.layerRequired.pow(52*(53**9))) ? "1" : formatNumber(n.div(tmp.layerRequired.pow(n.log(tmp.layerRequired).floor()))))
   + Layer(n.log(tmp.layerRequired))
-}
-function AbsLayerumNotationHMTL(n) {
-  return (n.gte(tmp.layerRequired.pow(52*(53**9))) ? "1" : formatNumber(n.div(tmp.layerRequired.pow(n.log(tmp.layerRequired).floor()))))
-  + "<small style=\"color: "
-  + rainbowTransition(n.add(tmp.layerRequired).log(tmp.layerRequired).mul(5).floor().root(1.5))
-  + ";\">"
-  + Layer(n.log(tmp.layerRequired))
-  + ".</small>"
 }
 function stats() {
   return "<p>"
