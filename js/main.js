3c88ba6db2df4c4a3359b0fccb8af7ffcb01fc1a
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-26 13:18:32 +0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const funcs = {
+  getSaveCode: function() {
+    return btoa(unescape(encodeURIComponent(JSON.stringify(tmp))));
+  },
+  saveGame: function(){
+    let str = funcs.getSaveCode();
+    localStorage.setItem("AbsoluteLayerumGameSave", str);
+  },
+  loadGame: function(importString) {
+    let loadVal = function(v, alt) {
+      return v !== undefined ? v : alt;
+    }
+
+    let item = importString !== undefined ? importString : localStorage.getItem("AbsoluteLayerumGameSave");
+    if(item !== null)
+    {
+      let obj;
+      try
+      {
+        obj = JSON.parse(decodeURIComponent(escape(atob(item))));
+      }
+        catch(e)
+      {
+        alert("Error: " + e);
+        return;
+      }
+      tmp.number = loadVal(E(obj.number), E(0));
+      tmp.multi = loadVal(E(obj.multi), E(0));
+      tmp.rank = loadVal(E(obj.rank), E(0));
+      tmp.prestige = loadVal(E(obj.prestige), E(0));
+      tmp.transcension = loadVal(E(obj.transcension), E(0));
+    }
+  }
+}
