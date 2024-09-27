0d468c979d1cabb2bda174fa5aa4cfebaf7f293e
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 18:15:50 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,10 +13,6 @@ let swears = ["arse", "arsehead", "arsehole", "ass", "asshole", "bastard", "bitc
               "sisterfucker", "slut", "spastic", "twat", "wanker"];
 let onCreate = function()
 {
-	game.numberUpgrades.push(
-		new NumberUpgrade("Stats Boost","Your numbers exponentiate!",E(5),E(1.1),E(1.1)),
-		new NumberUpgrade("Exponential Stats","Stat Exponent Go Brr",E(5),E(1.1),E(1.05)),
-	)
 	let initialGame = funcs.getSaveCode();
 	funcs.loadGame(initialGame);
 	funcs.update();
