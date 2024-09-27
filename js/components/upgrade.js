1e2f32ad99d432db2b733bcf202fb941a1cbaf44
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 11:18:23 +0500

    Update upgrade.js

--- js/components/upgrade.js
+++ js/components/upgrade.js
@@ -1,1 +1,25 @@
-
+Vue.component("upgrade", {
+	props: ["upgrade"],
+  methods: 
+    {
+      formatNumber: function(n, prec, prec1000, lim)
+      {
+        return funcs.formatNumber(n, prec, prec1000, lim)
+      }
+    },
+	computed:
+    {
+      canAfford: function()
+      {
+        return this.upgrade.getPrice().lt(tmp.number)
+      }
+    },
+	template:
+	`
+ 	<button onclick="upgrade.buy()" style="width: 200px; height: 150px;">
+ 		<div class="default center">{{upgrade.name}}</div>
+ 		<div class="small center">{{upgrade.desc}}</div>
+ 		<div class="tiny center">{{upgrade.desc}}</div>
+ 	</button>
+ 	`
+})
