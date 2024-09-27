5e67e60c9d023f124245b71ce3d7067ca8865c84
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 11:25:58 +0500

    Update upgrade.js

--- js/components/upgrade.js
+++ js/components/upgrade.js
@@ -1,25 +1,28 @@
 Vue.component("upgrade", {
 	props: ["upgrade"],
-  methods: 
-    {
-      formatNumber: function(n, prec, prec1000, lim)
-      {
-        return funcs.formatNumber(n, prec, prec1000, lim)
-      }
-    },
+	methods: 
+	{
+		formatNumber: function(n, prec, prec1000, lim)
+		{
+        	return funcs.formatNumber(n, prec, prec1000, lim)
+		},
+	
+	},
 	computed:
-    {
-      canAfford: function()
-      {
-        return this.upgrade.getPrice().lt(tmp.number)
-      }
-    },
+	{
+		canAfford: function()
+		{
+			return this.upgrade.getPrice().lt(tmp.number)
+      		}
+	},
 	template:
 	`
- 	<button onclick="upgrade.buy()" style="width: 200px; height: 150px;">
+ 	<button id="upgrade" onclick="upgrade.buy()" style="width: 250px; height: 180px;">
  		<div class="default center">{{upgrade.name}}</div>
  		<div class="small center">{{upgrade.desc}}</div>
- 		<div class="tiny center">{{upgrade.desc}}</div>
+ 		<div class="tiny center">Cost: {{formatNumber(upgrade.in_Cost.pow(upgrade.level), 2, 0, E(3003))}} Number</div>
+ 		<div class="tiny center">Level: {{upgrade.level}}</div>
+ 		<div class="tiny center">Effect: ^{{upgrade.getMultiplier()}}</div>
  	</button>
  	`
 })
