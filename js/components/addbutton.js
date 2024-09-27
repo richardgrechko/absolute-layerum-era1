fb0585046fcca18ed9023083d36bdbe53f939ee4
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 13:11:42 +0500

    Update addbutton.js

--- js/components/addbutton.js
+++ js/components/addbutton.js
@@ -2,7 +2,7 @@ Vue.component("addbutton", {
 	props: ["addbutton"],
 	template:
 	`
- 	<button id="addbutton" @click="addbutton.onClick" style="addbutton.style">
+ 	<button @click="addbutton.onClick" style="addbutton.style">
  		<div class="default center">{{upgrade.name}}</div>
  	</button>
  	`
