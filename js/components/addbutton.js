e5e02cf8ba9954659708bf50bbafd4adfca3a5d6
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 18:18:14 +0500

    Delete js/components/addbutton.js

--- js/components/addbutton.js
+++
@@ -1,9 +0,0 @@
-Vue.component("addbutton", {
-	props: ["addbutton"],
-	template:
-	`
- 	<button @click="addbutton.onClick" style="addbutton.style">
- 		<div class="default center">{{upgrade.name}}</div>
- 	</button>
- 	`
-})
