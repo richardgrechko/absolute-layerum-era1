7e7100c670570461c3309e8e484abf0d14aeeadf
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 11:53:41 +0500

    Delete js/components/button.js

--- js/components/button.js
+++
@@ -1,9 +0,0 @@
-Vue.component("button_", {
-	props: ["button_"],
-	template:
-	`
- 	<button id="upgrade" @click="{{button_.onClick}}" style="width: 250px; height: 180px; {{button_.style}}">
- 		<div class="default center">{{button_.text}}</div>
- 	</button>
- 	`
-})
