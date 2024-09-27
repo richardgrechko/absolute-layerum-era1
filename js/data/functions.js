5c7abdf06e62397499b475cad56f502629401072
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 17:50:29 +0500

    Update functions.js

--- js/data/functions.js
+++ js/data/functions.js
@@ -136,7 +136,7 @@ const funcs = {
 		dt2 = Date.now();
 		let dt = (dt2 - dt1) / 1000;
 		dt1 = Date.now();
-		game.number = game.number.mul(game.statsPerSecond.div(1/dt));
+		game.number = game.number.mul(game.statsPerSecond.pow(game.layerRequired/dt));
 		game.statsPerSecond = this.getStatProduction();
 		game.stats = `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"<p></div><div class="small center">Stats: </div><div class="default">{E(5).pow(game.number.log(game.layerRequired).sub(game.number.log(game.layerRequired).floor()))}</div><div class="small center" style="color: ${rainbowTransition(number.log(layerRequired).floor().log(1.05), 80, 70)}; text-shadow: 0 0 ${(game.number.gte(game.layerRequired.pow(100)) ? "10px" : `${game.number.log(game.layerRequired).floor().div(10)}px`)} ${rainbowTransition(game.number.log(game.layerRequired).floor().div(10), 60, 80)};">{{Layer(game.number.log(game.layerRequired).floor())}}</div><p><div class="tiny center">(+{{statsPerSecond}} stats/sec)</div>`
 		document.getElementById("tabs").innerHTML = game.tabs;
