43b6712760e7231c08ee5cb20719c31395780d5c
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 18:27:51 +0500

    Update numberupgrade.js

--- js/components/numberupgrade.js
+++ js/components/numberupgrade.js
@@ -1,5 +1,5 @@
-Vue.component("numberUpgrades", {
-	props: ["numberUpgrades"],
+Vue.component("numberupgrade", {
+	props: ["numberupgrade"],
 	methods: 
 	{
 		formatNumber: function(n, prec, prec1000, lim)
@@ -12,17 +12,17 @@ Vue.component("numberUpgrades", {
 	{
 		canAfford: function()
 		{
-			return this.numberUpgrade.getPrice().lt(tmp.number)
+			return this.numberupgrade.getPrice().lt(tmp.number)
       		}
 	},
 	template:
 	`
- 	<button :disabled="!canAfford" @click="numberUpgrades.buy()" style="background-color: #ff0; color: #0f0; width: 300px; height: 240px;">
- 		<div class="default center">{{numberUpgrades.name}}</div>
- 		<div class="small center">{{numberUpgrades.desc}}</div>
- 		<div class="tiny center">Cost: {{formatNumber(numberUpgrades.in_Cost.pow(numberUpgrades.level), 2, 0, E(3003))}} Number</div>
- 		<div class="tiny center">Level: {{formatNumber(numberUpgrades.level, 2, 0, E(3003))}}</div>
- 		<div class="tiny center">Effect: ^{{formatNumber(numberUpgrades.getMultiplier(), 2, 0, E(3003))}}</div>
+ 	<button :disabled="!canAfford" @click="numberupgrade.buy()" style="background-color: #ff0; color: #0f0; width: 300px; height: 240px;">
+ 		<div class="default center">{{numberupgrade.name}}</div>
+ 		<div class="small center">{{numberupgrade.desc}}</div>
+ 		<div class="tiny center">Cost: {{formatNumber(numberupgrade.in_Cost.pow(numberUpgrade.level), 2, 0, 3003)}} Number</div>
+ 		<div class="tiny center">Level: {{formatNumber(numberupgrade.level, 2, 0, 3003)}}</div>
+ 		<div class="tiny center">Effect: ^{{formatNumber(numberupgrade.getMultiplier(), 2, 0, 3003)}}</div>
  	</button>
  	`
 })
