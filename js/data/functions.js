117cd53c0d92608e17e78d238cf5f3d85d4f7293
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 15:31:34 +0500

    Update functions.js

--- js/data/functions.js
+++ js/data/functions.js
@@ -138,6 +138,7 @@ const funcs = {
 		dt1 = Date.now();
 		game.number = game.number.mul(game.statsPerSecond.div(1/dt));
 		game.statsPerSecond = this.getStatProduction();
+		game.stats = `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"<p></div><div class="small center">Stats: </div><div class="default">{E(5).pow(game.number.log(game.layerRequired).sub(game.number.log(game.layerRequired).floor()))}</div><div class="small center" style="color: ${rainbowTransition(number.log(layerRequired).floor().log(1.05), 80, 70)}; text-shadow: 0 0 ${(game.number.gte(game.layerRequired.pow(100)) ? "10px" : `${game.number.log(game.layerRequired).floor().div(10)}px`)} ${rainbowTransition(game.number.log(game.layerRequired).floor().div(10), 60, 80)};">{{Layer(game.number.log(game.layerRequired).floor())}}</div><p><div class="tiny center">(+{{statsPerSecond}} stats/sec)</div>`
 		document.getElementById("tabs").innerHTML = game.tabs;
 		document.getElementById("stats").innerHTML = game.stats;
 		document.getElementById("upgrades").innerHTML = [game.numberUpgrades.statBoost, game.numberUpgrades.poweringStats];
