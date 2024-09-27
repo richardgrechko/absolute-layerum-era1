ebc3e0fcad32aca3d77e66493795884ccb25b56c
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 10:16:33 +0500

    Update elements.js

--- js/main/elements.js
+++ js/main/elements.js
@@ -1,38 +1,43 @@
-var Elements = {
-  setHTML: function(text="Undefined?",element="small",style,vue=null) {
-    if (style.color == null) {
-      style.color = "#fff"
-    }
-    if (style.shadowX == null) {
-      style.shadowX = 0
-    }
-    if (style.shadowY == null) {
-      style.shadowY = 0
-    }
-    if (style.shadowBlur == null) {
-      style.shadowBlur = "#fff"
-    }
-    if (style.shadowColor == null) {
-      style.shadowColor = "#fff"
-    }
-    if (style.width == null) {
-      style.width = "100%"
-    }
-    return `<${element} ${vue} style="color: ${style.color}; text-shadow: ${style.shadowX} ${style.shadowY} ${style.shadowBlur} ${style.shadowColor}; width: ${style.width}">${text}</${element}>`
-  },
-  addButton: function(text="Undefined?",onClick=null,style) {
-    if (style.backgroundColor == null) {
-      style.backgroundColor = "#fff"
-    }
-    if (style.color == null) {
-      style.color = "#fff"
-    }
-    if (style.height == null) {
-      style.height = "200px"
-    }
-    if (style.width == null) {
-      style.width = "80px"
-    }
-    return `<center><button onclick="${onClick}" style="background-color: ${style.backgroundColor}; color: ${style.color}; height: ${style.height}; width: ${style.width}">${text}</button></center>`
-  }
+// Elements
+class Upgrades
+{
+	constructor(name, desc, in_Price, priceIncrease, production, multi)
+	{
+    	this.name = name;
+		this.desc = desc;
+		this.in_Price = in_Price;
+		this.priceIncrease = priceIncrease;
+		this.production = production;
+		this.multi = multi; // more stats per second (exponential)
+		this.level = 0;
+	}
+	
+	getMultiplier()
+	{
+		let base = this.multi.pow(this.level);
+		let softcap = base.gte(this.multi.pow(1000)) ? ((this.level+573) * 0.00063589192) : 1; // Here goes a softcap
+		return base.root(softcap);
+	}
+
+	getPrice()
+	{
+		let price = this.in_Price.mul(Decimal.pow(this.priceIncrease, this.level)));
+		let dilating = price.gte(Decimal.pow(2, 1024)) ? ((Decimal.log(price.div(E(Number.MAX_VALUE), 1e135)) / 2) + 1) : 1;
+		return price.pow(dilating);
+	}
+	
+	buy() {
+		if (this.getPrice.lt(tmp.number))
+		{
+			tmp.number = tmp.number.div(this.getPrice());
+			this.level++;
+			return true;
+		}
+		return false;
+	}
+	
+	buyMax()
+	{
+		while (this.buy());
+	}
 }
