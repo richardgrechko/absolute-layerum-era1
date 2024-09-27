41ac0fa7d7c6f2a4aa5dcfeb0ba1e3092b230bd8
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-27 22:42:29 +0500

    Update game.js

--- js/data/game.js
+++ js/data/game.js
@@ -8,11 +8,14 @@ var game = {
 		tab: "stats"
 	},
 	tabs: `<div onclick="funcs.setTab(1)" style="background-color: #999; color: #fff">Stats</div><div onclick="funcs.setTab(2)" style="background-color: #999; color: #fff">Upgrades</div><div onclick="funcs.setTab(3)" style="background-color: #999; color: #fff">Options</div>`,
-	stats: `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"<p></div><div class="small center">Stats: </div><div class="default">{E(5).pow(game.number.log(game.layerRequired).sub(game.number.log(game.layerRequired).floor()))}</div><div class="small center" style="color: {{rainbowTransition(number.log(layerRequired).floor().log(1.05), 80, 70)}}; text-shadow: 0 0 {{number.log(game.layerRequired).floor().div(10)}}em {{rainbowTransition(game.number.log(game.layerRequired).floor().div(10), 60, 80)}};">{{Layer(game.number.log(game.layerRequired).floor())}}, 0 0 {{number.log(game.layerRequired).floor().div(10)}}em {{rainbowTransition(game.number.log(game.layerRequired).floor().div(14), 60, 80)}};">{{Layer(game.number.log(game.layerRequired).floor())}}, 0 0 {{number.log(game.layerRequired).floor().div(10)}}em {{rainbowTransition(game.number.log(game.layerRequired).floor().div(20), 60, 80)}};">{{Layer(game.number.log(game.layerRequired).floor())}}</div><p><div class="tiny center">(+{{statsPerSecond}} stats/sec)</div>`,
+	stats: "",
 	numberUpgrades: [
-		new NumberUpgrade("Stats Boost","Your numbers exponentiate!",E(5),E(1.2),E(1.025)),
-		new NumberUpgrade("Exponential Stats","Stat Exponent Go Brr",E(5).pow(10),E(1.5),E(1.05)),
-		new NumberUpgrade("Exponential Stats","Stat Exponent Go Brr",E(5).pow(50),E(2),E(1.1)),
+		new NumberUpgrade("Stat Quickener 1","Your numbers exponentiate!",E(5),E(1.1),E(1.025)),
+		new NumberUpgrade("Stat Quickener 2","literal faster stats",E(5).pow(10),E(1.2),E(1.05)),
+		new NumberUpgrade("Stat Quickener 3","intense speed",E(5).pow(52),E(1.35),E(1.1)),
+		new NumberUpgrade("Stat Quickener 4","OH GOD",E(5).pow(260),E(1.5),E(1.15)),
+		new NumberUpgrade("Stat Quickener 5","ok thats too much bru",E(5).pow(260),E(2),E(1.2)),
+		new NumberUpgrade("Stat Quickener 6","I'm done.",E(5).pow(260),E(3),E(1.4)),
 	],
 	statsPerSecond: this.numberUpgrades[0].getMultiplier().mul(this.numberUpgrades[1].getMultiplier()),
 	options: `<div onclick="funcs.hardReset()" style="background-color: #f99; color: #f55">HARD RESET!</div>`,
