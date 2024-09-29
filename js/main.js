3a2ab16149a197e159666d78949687b5067e1461
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-29 13:11:01 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,10 +27,10 @@ function update()
 	dt1 = Date.now();
 	game.statsPerSecond = funcs.getStatProduction();
 	game.number = E(game.number).mul(game.layerRequirement.pow(game.statsPerSecond.mul(dt)));
-	game.stats = `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"<p></div><div class="small center">Stats: </div><div class="default">${E(5).pow(game.number.log(game.layerRequirement).sub(game.number.log(game.layerRequirement).floor()))}</div><div class="small center" style="color: ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().log(1.05), 80, 70)}; text-shadow: 0 0 ${game.number.log(game.layerRequirement).floor().div(141)}em currentcolor, 0 0 ${game.number.log(game.layerRequirement).floor().div(200)}em currentcolor, 0 0 ${game.number.log(game.layerRequirement).floor().div(100)}em currentcolor;">{{Layer(number.log(layerRequirement).floor())}}</div><p><div class="tiny center">(+{{statsPerSecond}} stats/sec)</div>`
+	game.stats = `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"<p></div><div class="small center">Stats: </div><div class="default center">${game.layerRequirement.pow(game.number.log(game.layerRequirement).sub(game.number.log(game.layerRequirement).floor()))}</div><div class="small center" style="color: ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().log(1.05), 80, 70)}; text-shadow: 0 0 ${game.number.log(game.layerRequirement).floor().div(100)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().log(1.05), 80, 70)}, 0 0 ${game.number.log(game.layerRequirement).floor().div(141)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().log(1.05), 80, 70)}, 0 0 ${game.number.log(game.layerRequirement).floor().div(200)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().log(1.05), 80, 70)};">${funcs.Layer(game.number.log(game.layerRequirement).floor())}</div><p><div class="tiny center">(+${game.statsPerSecond} stats/sec)</div>`
 	document.getElementById("tabs").innerHTML = game.tabs;
 	document.getElementById("stats").innerHTML = game.stats;
-	document.getElementById("upgrades").innerHTML = game.numberUpgrades;
+	document.getElementById("upgrades").innerHTML = game.numberUpgrades.split(",");
 	document.getElementById("options").innerHTML = game.options;
 	requestAnimationFrame(update);
 }
@@ -47,6 +47,8 @@ function onCreate()
 	initialGame = funcs.getSaveCode();
 
 	funcs.loadGame();
+
+	funcs.setTab(1);
 
 	requestAnimationFrame(update)
 }
