196a9adce1faf9353506e81cb1575cc57cdf39a4
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-29 16:02:04 +0500

    Update numberupgrade-table.js

--- js/components/numberupgrade-table.js
+++ js/components/numberupgrade-table.js
@@ -2,6 +2,7 @@ Vue.component("numberupgrade-table", {
 	props: ["numberupgrades"],
 	template: `
 	<table class="numberupgrade-table">
+ 		<div class="default center">Number Upgrades</div>
   		<numberupgrade v-for="(n, i) in numberupgrades" :numberupgrade="n" :key="i" ></numberupgrade>
 	</table>
 	`,
