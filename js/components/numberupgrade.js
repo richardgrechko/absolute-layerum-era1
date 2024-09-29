deaa51c9b3ae23a757ea258cff5beb7cdb80d607
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-29 14:06:20 +0500

    Update numberupgrade.js

--- js/components/numberupgrade.js
+++ js/components/numberupgrade.js
@@ -12,17 +12,17 @@ Vue.component("numberupgrade", {
 	{
 		canAfford: function()
 		{
-			return this.numberupgrade.getPrice().lt(tmp.number)
+			return this.numberupgrade.getPrice().lt(game.number)
       		}
 	},
 	template:
 	`
  	<button v-for="(n, i) in numberUpgrades" :numberupgrade="n" :key="i" :disabled="!canAfford" @click="numberupgrade.buy()" style="background-color: #ff0; color: #0f0; width: 300px; height: 240px;">
- 		<div class="default center">{{numberupgrade.name}}</div>
- 		<div class="small center">{{numberupgrade.desc}}</div>
- 		<div class="tiny center">Cost: {{formatNumber(numberupgrade.in_Cost.pow(numberUpgrade.level), 2, 0, 3003)}} Number</div>
- 		<div class="tiny center">Level: {{formatNumber(numberupgrade.level, 2, 0, 3003)}}</div>
- 		<div class="tiny center">Effect: ^{{formatNumber(numberupgrade.getMultiplier(), 2, 0, 3003)}}</div>
+ 		<div class="default center">{{numberUpgrades.name}}</div>
+ 		<div class="small center">{{numberUpgrades.desc}}</div>
+ 		<div class="tiny center">Cost: {{formatNumber(numberUpgrades.in_Cost.pow(numberUpgrades.level), 2, 0, 3003)}} Number</div>
+ 		<div class="tiny center">Level: {{formatNumber(numberUpgrades.level, 2, 0, 3003)}}</div>
+ 		<div class="tiny center">Effect: ^{{formatNumber(numberUpgrades.getMultiplier(), 2, 0, 3003)}}</div>
  	</button>
  	`
 })
