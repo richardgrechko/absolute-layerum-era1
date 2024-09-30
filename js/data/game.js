c2e010dcdf7337ce653fafc6a99b1469d6ff5da5
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 21:30:16 +0500

    Update game.js

--- js/data/game.js
+++ js/data/game.js
@@ -7,9 +7,9 @@ var game = {
 	setts: {
 		tab: "stats"
 	},
-	tabs: `<button class="tiny" onclick="funcs.setTab(1)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Stats</button><button class="tiny" onclick="funcs.setTab(2)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Upgrades</button><button class="tiny" onclick="funcs.setTab(3)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Update Logs</button><button class="tiny" onclick="funcs.setTab(4)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Options</button>`,
+	tabs: `<button onclick="funcs.setTab(1)">Stats</button><button onclick="funcs.setTab(2)">Upgrades</button><button onclick="funcs.setTab(3)">Update Logs</button><button onclick="funcs.setTab(4)>Options</button>`,
 	stats: "",
 	numberUpgrades: [],
 	statsPerSecond: E(0.1),
-	options: `<button onclick="funcs.hardReset()" style="background-color: #f99; color: #f55; width: 250px; height: 150px;">HARD RESET!</button>`,
+	options: `<button onclick="funcs.hardReset()" style="background-color: #f99; color: #f55;">HARD RESET!</button>`,
 };
