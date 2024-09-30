35073b72583b0d84be6f78e16bfe0239f7c172b1
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 21:52:54 +0500

    Update game.js

--- js/data/game.js
+++ js/data/game.js
@@ -5,11 +5,11 @@ var game = {
 	number: E(1),
 	layerRequired: E(5),
 	setts: {
-		tab: "stats"
+		exportString: ""
 	},
 	tabs: `<button onclick="funcs.setTab(1)">Stats</button><button onclick="funcs.setTab(2)">Upgrades</button><button onclick="funcs.setTab(3)">Update Logs</button><button onclick="funcs.setTab(4)">Options</button>`,
 	stats: "",
 	numberUpgrades: [],
 	statsPerSecond: E(0.1),
-	options: `<button onclick="funcs.hardReset()" style="background-color: #f99; color: #f55;">HARD RESET!</button><p><button onclick="funcs.exportSave()">Export</button><button onclick="funcs.exportSave()">Import</button><p><textarea rows="16" cols="64">Your exported save goes here</textarea>`,
+	options: `<button onclick="funcs.hardReset()" style="background-color: #f99; color: #f55;">HARD RESET!</button><p><button onclick="funcs.exportSave()">Export</button><button onclick="funcs.exportSave()">Import</button><p><textarea v-model="settings.exportString" rows="16" cols="64">Your exported save goes here</textarea>`,
 };
