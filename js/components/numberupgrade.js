5e9bd96b4bf47cb53ba232f212a6df2ad6e42b98
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 18:40:41 +0500

    Update numberupgrade.js

--- js/components/numberupgrade.js
+++ js/components/numberupgrade.js
@@ -21,7 +21,7 @@ Vue.component("numberupgrade", {
   		<button :disabled="!canAfford" @click="numberupgrade.buy()" style="background-color: #ff0; color: #0f0; width: 300px; height: 240px;">
  		<div class="small center">{{numberupgrade.name}}</div>
  		<div class="small center">{{numberupgrade.desc}}</div>
- 		<div class="tiny center">Cost: {{formatNumber(numberupgrade.in_Cost.pow(numberupgrade.level), 2, 0, 3003)}} Number</div>
+ 		<div class="tiny center">Cost: {{formatNumber(numberupgrade.getCost(), 2, 0, 3003)}} Number</div>
  		</button>
  		<div class="tiny center">Level: {{formatNumber(numberupgrade.level, 2, 0, 3003)}}</div>
  		<div class="tiny center">Effect: ^{{formatNumber(numberupgrade.getMultiplier(), 2, 0, 3003)}}</div>
