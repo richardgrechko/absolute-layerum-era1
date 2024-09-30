085473ddefee96797550773f9180f74b5c7d6533
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 22:34:28 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,7 @@ let layers = [
 	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 	" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 ]
-let grades = [
-	"ZFEDCBASQX",
-	["⇓", "↓", "÷", "--", "-", "", "+", "++", "×", "↑"]
-]
-let swears = ["arse", "arsehead", "arsehole", "ass", "asshole", "bastard", "bitch", "bloody", "bollocks", "brotherfucker", "bugger", "bullshit", "childfucker", "cock", "cocksucker", "crap", "cunt", 
+var swears = ["arse", "arsehead", "arsehole", "ass", "asshole", "bastard", "bitch", "bloody", "bollocks", "brotherfucker", "bugger", "bullshit", "childfucker", "cock", "cocksucker", "crap", "cunt", 
               "dammit", "damn", "damned", "dick", "dickhead", "dumbass", "dyke", "fatherfucker", "fuck", "fucker", "fucking", "gay", "goddammit", "goddamn", "goddamned", "goddamnit", "godsdamn", "hell", 
               "holyshit", "horseshit", "jackass", "jesuschrist", "kike", "motherfucker", "nigga", "nigger", "nigra", "pigfucker", "piss", "prick", "pussy", "shit", "shitass", "shite", "siblingfucker", "sisterfuck", 
               "sisterfucker", "slut", "spastic", "twat", "wanker"];
@@ -20,7 +16,8 @@ var app = new Vue({
 	methods: funcs,
 	created: onCreate,
 });
-game.layerRequirement = E(5);
+game.layerRequirement = E(5),
+game.prestigeRequirement = E(5).pow(52);
 function update()
 {
 	dt2 = Date.now();
@@ -28,7 +25,7 @@ function update()
 	dt1 = Date.now();
 	game.statsPerSecond = funcs.getStatProduction();
 	game.number = E(game.number).mul(game.layerRequirement.pow(game.statsPerSecond.mul(dt)));
-	game.stats = `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"</div><div style="text-align: center;"><div class="small"><img src="images/StatsImage.png">Stats: </text><text class="default">${funcs.formatNumber(game.layerRequirement.pow(game.number.log(game.layerRequirement).sub(game.number.log(game.layerRequirement).floor())))}</text><text class="small" style="color: ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), game.number.log(game.layerRequirement).floor().add(0.1).mul(10), 70)}; text-shadow: 0 0 ${game.number.log(game.layerRequirement).floor().div(100).min(1)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), 80, 70)}, 0 0 ${game.number.log(game.layerRequirement).floor().div(141).min(0.7)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), 80, 70)}, 0 0 ${game.number.log(game.layerRequirement).floor().div(200).min(0.5)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), 80, 70)};">${funcs.Layer(E(game.number).log(game.layerRequirement).floor())}</text></div><div class="tiny center">(+${funcs.formatNumber(game.statsPerSecond, 4, 0, 3003)} stats/sec)</div>`
+	game.stats = `<div class="small center" style="color: #900">Epilepsy warning when you get high stats! This is an inspiration of "SamirDevs AFK Incremental"</div><div style="text-align: center;"><div class="small"><img src="images/StatsImage.png">Stats: </text><text class="default">${funcs.formatNumber(game.layerRequirement.pow(game.number.log(game.layerRequirement).sub(game.number.log(game.layerRequirement).floor())))}</text><text class="small" style="color: ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), game.number.log(game.layerRequirement).floor().add(0.1).mul(10), 70)}; text-shadow: 0 0 ${game.number.log(game.layerRequirement).floor().div(100).min(1)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), 80, 70)}, 0 0 ${game.number.log(game.layerRequirement).floor().div(141).min(0.7)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), 80, 70)}, 0 0 ${game.number.log(game.layerRequirement).floor().div(200).min(0.5)}em ${funcs.rainbowTransition(game.number.log(game.layerRequirement).floor().root(2).log(1.01).add(1), 80, 70)};">${funcs.Layer(E(game.number).log(game.layerRequirement).floor())}</text></div><div class="tiny center">(+${funcs.formatNumber(game.statsPerSecond, 4, 0, 3003)} stats/sec)</div><div class="small" style="color: #48f">${game.prestige} Prestiges</div><presbutton :disabled="number.lte(prestigeRequirement)">Prestige</presbutton>`
 	document.getElementById("tabs").innerHTML = game.tabs;
 	document.getElementById("stats").innerHTML = game.stats;
 	if (keyMap.includes("m")) {
