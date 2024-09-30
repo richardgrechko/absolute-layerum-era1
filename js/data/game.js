9371dfc035e5ea6627bf4248b307225ff53691f6
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 19:22:40 +0500

    Update game.js

--- js/data/game.js
+++ js/data/game.js
@@ -7,7 +7,7 @@ var game = {
 	setts: {
 		tab: "stats"
 	},
-	tabs: `<button class="tiny" onclick="funcs.setTab(1)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Stats</button><button class="tiny" onclick="funcs.setTab(2)" style="background-color: #999; color: #fff; width: width: 150px; height: 100px;">Upgrades</button><button class="tiny" onclick="funcs.setTab(3)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Update Logs</button><button class="tiny" onclick="funcs.setTab(4)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Options</button>`,
+	tabs: `<button class="tiny" onclick="funcs.setTab(1)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Stats</button><button class="tiny" onclick="funcs.setTab(2)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Upgrades</button><button class="tiny" onclick="funcs.setTab(3)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Update Logs</button><button class="tiny" onclick="funcs.setTab(4)" style="background-color: #999; color: #fff; width: 150px; height: 100px;">Options</button>`,
 	stats: "",
 	numberUpgrades: [],
 	statsPerSecond: E(0.1),
