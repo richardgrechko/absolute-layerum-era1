08493e568b247745073e3d44f6be25317ab0dd9f
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 19:39:12 +0500

    Update numberupgrade-table.js

--- js/components/numberupgrade-table.js
+++ js/components/numberupgrade-table.js
@@ -22,7 +22,7 @@ Vue.component("numberupgrade-table", {
 	<table class="numberupgrade-table">
  		<div class="default center">Number Upgrades</div>
  		<div class="small center">Total Stat Production: {{formatNumber(totalMultiplier, 2, 0, 3003)}}</div>
-   		<button class="tiny center" @click="maxNumberUpgrades" style="background-color: #999; color: #bbb;">Max Upgrades</button>
+   		<button class="tiny center" onclick="maxNumberUpgrades" style="background-color: #999; color: #bbb;">Max Upgrades</button>
   		<numberupgrade v-for="(n, i) in numberupgrades" :numberupgrade="n" :key="i" ></numberupgrade>
 	</table>
 	`,
