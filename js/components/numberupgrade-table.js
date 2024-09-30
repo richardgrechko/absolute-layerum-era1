b43bd8b3aae748782e3b61d19216393e1fc9eaef
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-09-30 18:30:30 +0500

    Update numberupgrade-table.js

--- js/components/numberupgrade-table.js
+++ js/components/numberupgrade-table.js
@@ -1,8 +1,15 @@
 Vue.component("numberupgrade-table", {
 	props: ["numberupgrades"],
+	methods: {
+		formatNumber: (n, prec, prec1000, lim) => funcs.formatNumber(n, prec, prec1000, lim)
+	},
+	computed: {
+		totalMultiplier: () => functions.getMultiplier(),
+	},
 	template: `
 	<table class="numberupgrade-table">
  		<div class="default center">Number Upgrades</div>
+ 		<div class="small center">Total Stat Production: {{formatNumber(totalMultiplier, 2, 0, 3003)}}</div>
   		<numberupgrade v-for="(n, i) in numberupgrades" :numberupgrade="n" :key="i" ></numberupgrade>
 	</table>
 	`,
