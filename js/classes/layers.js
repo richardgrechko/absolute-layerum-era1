cd339fd9b10332136c4e790e2318cb9cebd52fbe
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-10-02 22:17:35 +0500

    Create layers.js

---
+++ js/classes/layers.js
@@ -0,0 +1,16 @@
+class LayerRequirement
+{
+	constructor(pow)
+	{
+		if (pow.lt(1))
+		{
+			this.layerRequirement = E(1)
+		} else if (pow.gte(1))
+		{
+			this.layerRequirement = E(5)
+		} else
+		{
+			this.layerRequirement = this.constructor(pow.sub(1)).mul(E(5).mul(E(1.01).pow(pow)))
+		}
+	}
+}
