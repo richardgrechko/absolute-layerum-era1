dc383042fe5f69e337749db123ff4ebefa838985
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-10-03 01:11:34 +0500

    Update layers.js

--- js/classes/layers.js
+++ js/classes/layers.js
@@ -7,7 +7,7 @@ class LayerRequirement
 			this.layerRequirement = E(1)
 		} else if (pow.gte(1))
 		{
-			this.layerRequirement = E(5)
+			this.layerRequirement = E(5).pow(pow)
 		} else
 		{
 			this.layerRequirement = this.constructor(pow.sub(1)).mul(E(5).mul(E(1.01).pow(pow)))
